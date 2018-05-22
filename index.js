f0cd4411274c6fcfe8b6e9ce18e0e8a5e8db40be
Arnout Kazemier <info@3rd-Eden.com>
2018-05-22 12:23:41 +0200

    [minor] Moved the resolve logic to it's own package

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var resolve = require('resolves');
 var path = require('path');
 var fs = require('fs');
 
@@ -38,39 +39,6 @@ function parse(file) {
 
   try { return JSON.parse(str); }
   catch (e) { return null; }
-}
-
-/**
- * Actual function that tries to resolve a given directory in search
- * of a given what ever triggers the iterator to return a value.
- *
- * @param {String} root Directory that we need to search in.
- * @param {Function} iterator Function that is called on each directory level.
- * @returns {Mixed} Null incase of nothing, anthing else incase of result.
- * @private
- */
-function resolve(root, iterator) {
-  /**
-   * The actual function that does the iteration of the directory and
-   * searches for the correct file. It traverses the parent directory
-   * until it reaches the root directory.
-   *
-   * @private
-   */
-  return (function next() {
-    if (root.match(/^(\w:\\|\/)$/) || root == path.sep) return null;
-
-    var data = iterator(root);
-    if (data) return data;
-
-    root = path.resolve(root, '..');
-
-    //
-    // No suitable match found, continue with the iteration until we've found
-    // something fruitful.
-    //
-    return next();
-  }());
 }
 
 /**
